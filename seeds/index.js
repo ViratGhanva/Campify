7012e038ebe8e7788b4714e517862a206664b314
ViratGhanva <viratghanva@gmail.com>
2023-10-26 22:33:00 +0530

    Adding Basic Styles

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
-const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect("mongodb://127.0.0.1:27017/Campify");
 /*useNewUrlParser,
@@ -17,22 +17,25 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
 
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "http://source.unsplash.com/collection/484351",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam voluptatum accusamus architecto numquam, consequatur corrupti sapiente nesciunt atque animi? Numquam cum placeat est animi, error ipsum aliquam tenetur similique fuga!",
+      price
     });
     await camp.save();
   }
 };
-
 
 seedDB().then(() => {
   mongoose.connection.close();
