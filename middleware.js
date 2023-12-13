a169c9d9a1c0b09d6740c207da057928808d719d
ViratGhanva <viratghanva@gmail.com>
2023-12-13 11:49:07 +0530

    Fix error

--- middleware.js
+++ middleware.js
@@ -29,15 +29,17 @@ module.exports.isAuthor = async (req, res, next) => {
         req.flash('error', 'you do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
+    next();
 }
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    const { id,reviewId } = req.params;
+    const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'you do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
+    next();
 }
 
 module.exports.validateReview = (req, res, next) => {
